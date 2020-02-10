e3fd58ef233a5d9289691fae06191eeefc6f315b
qianhao <ianloe666@gmail.com>
2020-02-09 19:39:10 -0600

    keep record

--- src/Components/History.js
+++ src/Components/History.js
@@ -9,13 +9,27 @@ function History() {
             <hr />
 
             <div className='project-container about'>
-                <h2> Added About Page and Router </h2>
+                <h2> 02/08 2020 </h2>
                 <div className="skill-time">
-                    <p className="project-skill"> React Bootstrap, Router, HashRouter </p>
-                    <p className="project-time">02/07 2020</p>
+                    <p className="project-skill"> image-webpack-loader </p>
+                    <p className="project-time"></p>
                 </div>
                 <div className="project-description">
                     <ul>
+                        <li> Added 'image-webpack-loader' into webpack.config.js, in order to compress images.</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className='project-container about'>
+                <h2> 02/07 2020 </h2>
+                <div className="skill-time">
+                    <p className="project-skill"> React Bootstrap, Router, HashRouter </p>
+                    <p className="project-time"></p>
+                </div>
+                <div className="project-description">
+                    <ul>
+                        <li> Added About Page and Router </li>
                         <li> So that I could have multiple pages on my portfolio, this website may be updated in the future</li>
                     </ul>
                 </div>
@@ -23,26 +37,29 @@ function History() {
 
 
             <div className='project-container about'>
-                <h2> Added icon of info, and ToolTips </h2>
+                <h2> 02/05 2020 </h2>
                 <div className="skill-time">
                     <p className="project-skill"> Font-Awesome, and Metrial-UI </p>
-                    <p className="project-time">02/05 2020</p>
+                    <p className="project-time"></p>
                 </div>
                 <div className="project-description">
                     <ul>
+                        <li> Added icon of info, and ToolTips </li>
                         <li> Customized ToolTips </li>
                     </ul>
                 </div>
             </div>
 
             <div className='project-container about'>
-                <h2> Added Avatar, and Projects </h2>
+                <h2> 02/04 2020 </h2>
                 <div className="skill-time">
                     <p className="project-skill"> React Bootstrap </p>
-                    <p className="project-time">02/04 2020</p>
+                    <p className="project-time"></p>
                 </div>
                 <div className="project-description">
-                    {/* <p> • Customized ToolTips </p> */}
+                    <ul>
+                        <li> Added Avatar, and Projects </li>
+                    </ul>
                 </div>
             </div>
 

8b53e2cb1d4f941644708efe3c296914c3988585
qianhao <ianloe666@gmail.com>
2020-08-13 17:48:33 -0500

    clear

--- src/JS/deployDriver.js
+++ src/JS/deployDriver.js
@@ -36,8 +36,6 @@ window.onload = function () {
     console.log('============')
     let check = new CheckCookie();
     console.log('Your cookie will be stored for 1 day')
-    // console.log('here, the check:', check)
-    // console.log('check.checkCookie():', check.checkCookie())
     if (check.checkCookie()){
         console.log('driver, called')
         // driver();
